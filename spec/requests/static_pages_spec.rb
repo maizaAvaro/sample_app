82b688aeef6a22c980b065840ebae5a9add2921c
Nathan Ray <nathan@nathan-macpro.man.co>
2014-07-22 14:04:00 -0400

    Add a full_title helper and other tutorial steps

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe 'Static pages' do
+
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
+  describe 'Home page' do
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_selector('h1', :text => 'Sample App')
+    end
+    it 'should have the base title' do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it 'should not have the custom page title' do
+      visit '/static_pages/home'
+      expect(page).to_not have_title("#{base_title} | Home")
+    end
+  end
+
+  describe 'Help page' do
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_selector('h1', :text => 'Help')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
+  describe 'About page' do
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_selector('h1', :text => 'About Us')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe 'Contact page' do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('h1', :text => 'Contact')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+end
